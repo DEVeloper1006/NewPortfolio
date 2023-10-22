c63d2d653a12daca81e2c6d305a60db7fd03bad1
Dev Mody <devmody@Devs-MacBook-Pro-2.local>
2023-10-22 19:21:53 -0400

    Finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../style'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion';
+
+const ServiceCard = ({index, title, icon}) => {
+  return (
+    <p>{title}</p>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+          I'm a 2nd Year Honours Computer Science (Co-Op) Student @ McMaster University. I specialize in Front-End Development, where I am skilled in HTML, CSS and JavaScript alongside React and Next.JS.
+          I'm also proficient in Python, C, C++, and Java alongside Functional Programming Languages like Haskell.
+          I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems! 
+      </motion.p>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => {
+          <ServiceCard key={service.title} index={index} {...service}/>
+        })}
+      </div>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,15 +26,15 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
-            <div className='sm:hideen flex flex-1 justify-end items-center'>
+            <div className='sm:hidden flex flex-1 justify-end items-center'>
                 <img src={toggle ? close : menu} alt='menu' className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => setToggle(!toggle)}></img>
             </div>
-            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 bg-opacity-10 bg-slate-200 bg-blur-md absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
                 <ul className='list-none flex justify-end items-start flex-col gap-4'>
                    {navLinks.map((nav) => (
-                    <li key={nav.id} className={`${ active === nav.title ? "text-white" : "text-secondary"} hover:text-white font-poppins font-medium cursor-pointer text-[16px]`} onClick={() => {
+                    <li key={nav.id} className={`${ active === nav.title ? "text-white" : "text-secondary"} hover:text-white font-poppins font-medium cursor-pointer text-[18px] transition ease-in`} onClick={() => {
                       setToggle(!toggle);
-                      setActive(link.title);
+                      setActive(nav.title);
                     }}>
                         <a href={`#${nav.id}`}>{nav.title}</a>
                     </li>
